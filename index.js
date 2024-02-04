5dc1da851dc059d8d580075192d9eb4079e55cd1
diegoleondev <diegoleon.dev@gmail.com>
2024-02-03 18:51:21 -0600

    Pony App

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const https = require("https");
+const fs = require("fs");
+const express = require("express");
 
 const app = express();
+
+const puerto = 8000;
+
+const privateKey = fs.readFileSync(
+  "/etc/letsencrypt/live/api.tupony.app/privkey.pem",
+  "utf8"
+);
+const certificate = fs.readFileSync(
+  "/etc/letsencrypt/live/api.tupony.app/cert.pem",
+  "utf8"
+);
+const ca = fs.readFileSync(
+  "/etc/letsencrypt/live/api.tupony.app/chain.pem",
+  "utf8"
+);
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca,
+};
+
+const httpsServer = https.createServer(credentials, app);
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'src/public')));
+app.use(express.static(path.join(__dirname, "src/public")));
 
-app.listen(3002);
-
-console.log('Server on port http://localhost:3002');
+httpsServer.listen(puerto, () => {
+  console.log(`Servidor HTTPS en puerto ${puerto}`);
+});
